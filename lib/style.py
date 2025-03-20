c5c2a775b31f49d5df8829c648696a54bf61d6cc
Axel <nigthmaresans2@gmail.com>
2025-03-19 23:05:42 -0600

    lib wasn't uploading cuz the gitignore template xd

---
+++ lib/style.py
@@ -0,0 +1,22 @@
+import subprocess
+
+from lib.utils import Watcher
+from lib.constants import CONFIG_DIR
+
+print("Copy/link the style directory to ~/.config/shell/style pls")
+
+class Style:
+    STYLES_DIR = CONFIG_DIR / "style"
+    def compile_scss():
+        try:
+            subprocess.check_call(["sass", str(Style.STYLES_DIR / "scss" / "main.scss"), str(Style.STYLES_DIR / "style.css")], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        except Exception as e:
+            print(e)
+            return
+        
+    def watcher(cb):
+        w = Watcher()
+        w.add_watch(str(Style.STYLES_DIR / "scss"))
+        w.add_watch(str(Style.STYLES_DIR / "scss" / "widgets"))
+        w.connect("event", cb)
+        w.start()
