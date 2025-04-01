584a19f9e2e5b724555517ac536b5f068e73127b
Axel <nigthmaresans2@gmail.com>
2025-03-31 21:29:55 -0600

    removed menus folder

--- widgets/quick/buttons/network.py
+++ widgets/quick/buttons/network.py
@@ -1,8 +1,89 @@
-from gi.repository import Gtk, AstalNetwork
-
-from widgets.quick.menus.network import QuickNetworkMenu
+from widgets.prompts.network import NetworkPrompt
 from widgets.quick.icons import NetworkIndicator
 from widgets.custom.buttons import QuickButton
+from widgets.custom.pages import StatusPage
+from lib.utils import Box, getLogger
+from lib.network import NWrapper
+
+from gi.repository import Gtk, AstalNetwork
+
+class WifiButton(Gtk.Button):
+    def __init__(self, access_point: AstalNetwork.AccessPoint, active_ssid: str):
+        super().__init__()
+        self.ap = access_point
+        self.content = Box(spacing=10, hexpand=True)
+        self.icon = Gtk.Image(pixel_size=16,icon_name=access_point.get_icon_name())
+        self.name = Gtk.Label(label=access_point.get_ssid(), hexpand=True, xalign=0)
+
+        self.content.append_all([self.icon, self.name])
+
+        if active_ssid == access_point.get_ssid():
+            self._connected = Gtk.Image(icon_name="emblem-ok", pixel_size=16, \
+                                        halign=Gtk.Align.END, visible=active_ssid == access_point.get_ssid())
+            self.content.append(self._connected)
+            self.add_css_class("active-wifi")
+
+        self.set_child(self.content)
+        self.connect("clicked", self.__on_clicked)
+    
+    def __on_clicked(self, _):
+        p = NetworkPrompt(self.ap)
+        p.present()
+
+class QuickNetworkMenu(Box):
+    def __init__(self):
+        super().__init__(vertical=True, spacing=4)
+        self.logger = getLogger("QuickNetworkMenu")
+        self.wrapper = NWrapper.get_default()
+
+        self.placeholder = StatusPage()
+        self.append(self.placeholder)
+        
+        self.wrapper.connect("changed", self.__on_wrapper_change); self.__on_wrapper_change(None)
+        self.connect("notify::children", self.__on_children_change)
+
+    def __on_children_change(self, *_):
+        if len(self.children) == 1:
+            self.show_zero_wifi_placeholder()
+        else:
+            self.placeholder.set_visible(False)
+
+    def __on_wrapper_change(self, _):
+        if self.wrapper.is_wired():
+            self.logger.info("Detected wired connection")
+            self.show_wired_placeholder()
+        elif self.wrapper.is_wifi() is False:
+            self.logger.info("Detected no wifi device")
+            self.show_no_wifi_device_placeholder()
+        else:
+            self.logger.info("Detected wifi device")
+            self.wrapper.wifi.scan()
+            self.wrapper.wifi.connect("notify::access-points", self.__on_access_points_changed); self.__on_access_points_changed()
+            self.placeholder.set_visible(False)
+        
+    def __on_access_points_changed(self, *_):
+        w = [WifiButton(a, self.wrapper.ssid) for a in self.wrapper.wifi.get_access_points() if a.get_ssid() is not None]
+        self.clear()
+        self.append_all(w)
+
+    def show_zero_wifi_placeholder(self):
+        self.placeholder.set_title("No wifi nearby")
+        self.placeholder.set_description("No wifi devices to connect")
+        self.placeholder.set_icon_name("network-wireless-no-route-symbolic")
+        self.placeholder.set_visible(True)
+    
+    def show_wired_placeholder(self):
+        self.placeholder.set_title("Wired connection")
+        self.placeholder.set_description("The pc is connected to a wired network")
+        self.placeholder.set_icon_name("network-wired-symbolic")
+        self.placeholder.set_visible(True)
+    
+    def show_no_wifi_device_placeholder(self):
+        self.logger.info("Showing no wifi device placeholder")
+        self.placeholder.set_title("No wifi device")
+        self.placeholder.set_description("Connect a wifi dongle")
+        self.placeholder.set_icon_name("network-wireless-disabled-symbolic")
+        self.placeholder.set_visible(True)
 
 class QuickNetwork(QuickButton):
     def __init__(self):
