8919c6c0708cda9e473a4b55f5a9ec1b79000b2b
Axel <nigthmaresans2@gmail.com>
2025-06-21 01:13:23 -0600

    better looking bar

--- xtreme_shell/components/bar.py
+++ xtreme_shell/components/bar.py
@@ -1,24 +1,31 @@
-from gi.repository import Gtk, Astal, AstalHyprland, GLib
+from gi.repository import Gtk, Astal, AstalHyprland, GLib, Pango
 from xtreme_shell.widgets.window import XtremeWindow
 from xtreme_shell.modules.config import Bar as BarConfig
 from xtreme_shell.widgets.box import Box
 
-
-class HyprWorkspaces(Gtk.Label):
-    def __init__(self):
-        super().__init__(css_classes=["container"])
+class HyprProp(Gtk.Label):
+    def __init__(self, prop: str, func, **kwargs):
+        super().__init__(css_classes=["bar-container"], **kwargs)
         hypr = AstalHyprland.Hyprland.get_default()
-        hypr.connect("notify::focused-workspace", self.__on_workspace)
+        hypr.connect(f"notify::{prop}", func)
+        func(hypr)
+    
+
+class HyprWorkspaces(HyprProp):
+    def __init__(self):
+        super().__init__("focused-workspace", self.__on_workspace)
 
     def __on_workspace(self, hypr, *_):
         self.set_label(f"Workspace {hypr.props.focused_workspace.get_id()}")
 
 
-class HyprWindow(Gtk.Label):
+class HyprWindow(HyprProp):
     def __init__(self):
-        super().__init__(css_classes=["container"])
-        hypr = AstalHyprland.Hyprland.get_default()
-        hypr.connect("notify::focused-client", self.__on_client)
+        super().__init__(
+            "focused-client", 
+            self.__on_client,
+            max_width_chars=30,
+            ellipsize=Pango.EllipsizeMode.END)
 
     def __on_client(self, hypr, *_):
         if hypr.props.focused_client is None:
@@ -29,7 +36,7 @@ class HyprWindow(Gtk.Label):
 
 class DateTime(Gtk.Label):
     def __init__(self):
-        super().__init__(css_classes=["container"])
+        super().__init__(css_classes=["bar-container"])
 
         GLib.timeout_add(1000, self.__change_date)
 
@@ -46,7 +53,6 @@ class Bar(XtremeWindow):
             "top",
             ["top", "left", "right"],
             exclusivity=Astal.Exclusivity.EXCLUSIVE,
-            css_classes=["bar"]
         )
 
         self.logger.info("Initializing...")
@@ -56,14 +62,14 @@ class Bar(XtremeWindow):
         self.present()
 
     def setup_widgets(self):
-        content = Gtk.CenterBox()
+        content = Gtk.CenterBox(css_classes=["bar-box"])
 
-        left_box = Box()
+        left_box = Box(spacing=10)
         workspaces = HyprWorkspaces()
         window = HyprWindow()
         left_box.append(workspaces, window)
 
-        end_box = Box()
+        end_box = Box(spacing=10)
         date = DateTime()
 
         end_box.append(date)
