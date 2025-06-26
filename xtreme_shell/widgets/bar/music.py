c7c86e35c0f286c47262275eb459be4fa18ced24
Axel <nigthmaresans2@gmail.com>
2025-06-26 17:26:46 -0600

    added music popover

--- xtreme_shell/widgets/bar/music.py
+++ xtreme_shell/widgets/bar/music.py
@@ -1,6 +1,7 @@
-from gi.repository import AstalMpris, Gtk, GLib
+from gi.repository import AstalMpris, Gtk, GLib, Pango, GObject
 from xtreme_shell.modules.config import BarMusic
-from xtreme_shell.widgets import Widget
+from xtreme_shell.widgets.box import Box
+from xtreme_shell.widgets.cava import Cava
 import logging
 
 
@@ -14,10 +15,16 @@ def to_minutes(seconds):
 class BlurryImage(Gtk.Picture):
     def __init__(self, blur: int | float = 0, **kwargs):
         super().__init__(**kwargs)
-        self.blur = blur
+        self.__blur = blur
 
-    def set_blur(self, blur):
-        self.blur = blur
+    @GObject.Property
+    def blur(self):
+        return self.__blur
+
+    @blur.setter
+    def blur(self, blur):
+        self.__blur = blur
+        self.notify("blur")
 
     def do_snapshot(self, snapshot):
         snapshot.push_blur(self.blur)
@@ -25,12 +32,106 @@ class BlurryImage(Gtk.Picture):
         snapshot.pop()
 
 
+class MusicPopover(Gtk.Popover):
+    player: AstalMpris.Player = GObject.Property(type=AstalMpris.Player)
+
+    def __init__(self):
+        super().__init__(has_arrow=False, width_request=350, height_request=150)
+        self.set_offset(0, 10)
+        frame = Gtk.Frame.new()
+        overlay = Gtk.Overlay.new()
+
+        self.background = BlurryImage(
+            blur=20,
+            content_fit=Gtk.ContentFit.COVER,
+        )
+
+        self.cava = Cava()
+
+        self.opacity_box = Box(hexpand=True, vexpand=True)
+        self.opacity_box.set_css("background-color: colors.$surface-container")
+
+        controls = self.__create_box(vertical=True, spacing=10)
+        label_box = self.__create_box(vertical=True)
+
+        self.title = Gtk.Label(
+            css_classes=["title-1"],
+            max_width_chars=45,
+            justify=Gtk.Justification.CENTER,
+            ellipsize=Pango.EllipsizeMode.MIDDLE,
+        )
+        self.artist = Gtk.Label(css_classes=["title-4"])
+
+        label_box.append(self.title, self.artist)
+
+        button_box = self.__create_box(spacing=5)
+
+        previous = self.__create_button("media-skip-backward-symbolic", self.__prev)
+        self.play_button = self.__create_button(
+            "media-playback-start-symbolic", self.__play_pause
+        )
+        next = self.__create_button("media-skip-forward-symbolic", self.__next)
+
+        button_box.append(previous, self.play_button, next)
+
+        controls.append(label_box, button_box)
+
+        overlay.add_overlay(self.background)
+        overlay.add_overlay(self.cava)
+        overlay.add_overlay(self.opacity_box)
+        overlay.add_overlay(controls)
+
+        BarMusic.blur.bind(self.background, "blur")
+        BarMusic.opacity.bind(self.opacity_box, "opacity")
+
+        frame.set_child(overlay)
+        self.set_child(frame)
+
+    def __create_box(self, **kwargs):
+        return Box(halign=Gtk.Align.CENTER, valign=Gtk.Align.CENTER, **kwargs)
+
+    def __create_button(self, icon, func):
+        b = Gtk.Button(icon_name=icon)
+        b.connect("clicked", func)
+        return b
+
+    def __prev(self, _):
+        self.player.previous()
+
+    def __play_pause(self, _):
+        self.player.play_pause()
+
+    def __next(self, _):
+        self.player.next()
+
+    def set_player(self, player):
+        self.player = player
+        self.player.connect("notify::playback-status", self.__update_playback_status)
+        self.__update_playback_status()
+
+    def __update_playback_status(self, *_):
+        if self.player.get_playback_status() == AstalMpris.PlaybackStatus.PLAYING:
+            self.play_button.set_icon_name("media-playback-pause-symbolic")
+        elif self.player.get_playback_status() == AstalMpris.PlaybackStatus.PAUSED:
+            self.play_button.set_icon_name("media-playback-start-symbolic")
+
+    def update(self):
+        self.background.set_filename(self.player.get_cover_art())
+        self.title.set_label(self.player.get_title())
+        self.artist.set_label(self.player.get_artist())
+
+
 class MusicLabel(Gtk.Overlay):
     def __init__(self):
-        super().__init__()
+        super().__init__(css_classes=["clickable"])
+        gesture = Gtk.GestureClick.new()
+        self.add_controller(gesture)
+
         self.ids = []
         self.logger = logging.getLogger("MusicLabel")
         self.player = None
+        self.popover = MusicPopover()
+        self.popover.set_parent(self)
 
         self.background = BlurryImage(
             css_classes=["bar-container"],
@@ -40,11 +141,16 @@ class MusicLabel(Gtk.Overlay):
         )
 
         self.label = Gtk.Label(margin_start=5, margin_end=5)
-        self.opacity_box = Gtk.Box(hexpand=True, vexpand=True)
+        self.opacity_box = Box(hexpand=True, vexpand=True)
+        self.opacity_box.set_css(
+            "border-radius: 4px; background-color: colors.$surface-container"
+        )
 
-        BarMusic.blur.on_change(lambda x: self.background.set_blur(x), once=True)
-        BarMusic.opacity.on_change(self.__change_opacity, once=True)
+        BarMusic.blur.bind(self.background, "blur")
+        BarMusic.opacity.bind(self.opacity_box, "opacity")
         BarMusic.player.on_change(self.__set_player, once=True)
+
+        gesture.connect("released", self.__popup)
 
         self.add_overlay(self.background)
         self.add_overlay(self.opacity_box)
@@ -52,12 +158,8 @@ class MusicLabel(Gtk.Overlay):
 
         self.set_measure_overlay(self.label, True)
 
-    def __change_opacity(self, op):
-        Widget.set_css(
-            self.opacity_box,
-            f"border-radius: 4px;\
-            background-color: rgba(colors.$surface_container, {op});",
-        )
+    def __popup(self, *_):
+        self.popover.popup()
 
     def __change_visible(self, *_):
         if self.player.get_available() is False:
@@ -69,12 +171,14 @@ class MusicLabel(Gtk.Overlay):
     def __set_player(self, name):
         self.logger.info(f"Changing player to {name}...")
         self.player = AstalMpris.Player.new(name)
+        self.popover.set_player(self.player)
 
         self.player.connect("notify::available", self.__change_visible)
         self.player.connect("notify", self.__update)
 
     def __update(self, *_):
         self.__change_visible()
+        self.popover.update()
         if (a := self.player.get_cover_art()) not in ["", None]:
             self.background.set_filename(a)
 
