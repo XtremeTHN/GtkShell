d66c3551fdf47b9110900d58754589ac26081244
Axel <nigthmaresans2@gmail.com>
2025-10-04 20:32:14 -0600

    fix: wifi icon not binding

--- xtreme_shell/widgets/icons/network.py
+++ xtreme_shell/widgets/icons/network.py
@@ -1,4 +1,5 @@
 from gi.repository import AstalNetwork, Gtk, GObject
+import logging
 
 
 class NetworkIcon(Gtk.Image):
@@ -7,6 +8,7 @@ class NetworkIcon(Gtk.Image):
     def __init__(self, size=16):
         super().__init__(pixel_size=size)
 
+        self.logger = logging.getLogger("NetworkIcon")
         self.net = AstalNetwork.get_default()
 
         self.net.connect("notify", self.change_obj)
@@ -25,12 +27,16 @@ class NetworkIcon(Gtk.Image):
 
     def change_obj(self, *_):
         device = None
-        if (device := self.net.get_wifi()) is not None:
+        if (v := self.net.get_wifi()) is not None:
+            device = v
             device.bind_property(
                 "ssid", self, "tooltip-text", GObject.BindingFlags.SYNC_CREATE
             )
+            self.logger.info("Wifi device detected")
 
-        if (device := self.net.get_wired()) is not None:
+        if (v := self.net.get_wired()) is not None:
+            device = v
+
             device.bind_property(
                 "state",
                 self,
@@ -39,7 +45,12 @@ class NetworkIcon(Gtk.Image):
                 transform_to=self.on_state_change,
             )
 
+            self.logger.info("Wired device detected")
+            if device:
+                self.logger.info("Replaced wifi with ethernet")
+
         if device is not None:
             device.bind_property(
                 "icon-name", self, "icon-name", GObject.BindingFlags.SYNC_CREATE
             )
+            self.logger.info("Icon binded")
